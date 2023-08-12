5cf9c897408e92c4544c67a975accc78627543ec
RashadRZ <rashadzulkarnaen@gmail.com>
2023-08-12 20:25:11 +0700

    Queue functions done

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
+# Queue
+queue = []
+queue_number = 1
+
+
+# UI functions
+def call_counter1():
+    if len(queue) != 0:
+        call = queue.pop(0)
+        counter1.configure(text=call)
+        show_next()
+
+
+def call_counter2():
+    if len(queue) != 0:
+        call = queue.pop(0)
+        counter2.configure(text=call)
+        show_next()
+
+
+def show_next():
+    if len(queue) != 0:
+        next_queue.configure(text=f'{queue[0]}')
+    else:
+        next_queue.configure(text='')
+
+
+def take_ticket():
+    global queue_number
+    queue.append(f'{queue_number:03d}')
+    queue_number += 1
+    show_next()
+
+
 # window setup
 root = Tk()
 root.title('Health Care')
@@ -19,26 +53,24 @@ root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 root.resizable(False, False)
 root.configure(bg='white')
 
-# UI component
+# UI widget
 top_frame = Frame(root, bg='white')
-header_label = Label(top_frame, text='HEALTH CARE', font=('Arial', 28, 'bold'), bg='white')
+header_label = Label(top_frame, text='IOSEFKA\'S CLINIC', font=('Arial', 28, 'bold'), bg='white')
 my_image = ImageTk.PhotoImage(Image.open('images/hospital.png'))
 header_image = Label(top_frame, image=my_image, bg='white')
 
 middle_frame = Frame(root, bg='white')
 counter1_label = Label(middle_frame, text='TICKET COUNTER 1', font=('Arial', 18, 'bold'), bg='white', height=2)
-counter1 = Button(middle_frame, text='B001', font=('Arial', 26, 'bold'), bg='black', fg='white', borderwidth=0, width=16, height=4)
+counter1 = Button(middle_frame, command=call_counter1, text='', font=('Arial', 26, 'bold'), bg='black', fg='white', borderwidth=0, width=16, height=4)
 
 counter2_label = Label(middle_frame, text='TICKET COUNTER 2', font=('Arial', 18, 'bold'), bg='white', height=2)
-counter2 = Button(middle_frame, text='B002', font=('Arial', 26, 'bold'), bg='black', fg='white', borderwidth=0, width=16, height=4)
+counter2 = Button(middle_frame, command=call_counter2, text='', font=('Arial', 26, 'bold'), bg='black', fg='white', borderwidth=0, width=16, height=4)
 
 next_queue_label = Label(middle_frame, text='NEXT', font=('Arial', 18, 'bold'), bg='white', height=2)
-next_queue = Button(middle_frame, text='A001', font=('Arial', 26, 'bold'), bg='white', fg='black', highlightthickness=1, highlightbackground='black', highlightcolor='black', borderwidth=0, width=16, height=4, default='active')
-next_queue.configure()
+next_queue = Button(middle_frame, text='', font=('Arial', 26, 'bold'), bg='white', fg='black', highlightthickness=1, highlightbackground='black', highlightcolor='black', borderwidth=0, width=16, height=4, default='active')
 
 bottom_frame = Frame(root, bg='white')
-priority = Button(bottom_frame, text='PRIORITY', font=('Arial', 18, 'bold'), bg='#E20016', fg='white', borderwidth=0, width=11)
-regular = Button(bottom_frame, text='REGULAR', font=('Arial', 18, 'bold'), bg='white', fg='black', highlightthickness=1, highlightbackground='#E20016', highlightcolor='#E20016', borderwidth=0, width=11, default='active')
+take_ticket_button = Button(bottom_frame, command=take_ticket, text='TAKE TICKET', font=('Arial', 18, 'bold'), bg='#E20016', fg='white', borderwidth=0, width=11)
 
 # UI positioning
 top_frame.pack(pady=50)
@@ -56,8 +88,7 @@ next_queue_label.grid(row=0, column=2)
 next_queue.grid(row=1, column=2, padx=30)
 
 bottom_frame.pack(pady=50)
-priority.grid(row=0, column=0, padx=10, pady=10)
-regular.grid(row=0, column=1, padx=10, pady=10)
+take_ticket_button.grid(row=0, column=0, padx=10, pady=10)
 
 # run
 root.mainloop()
